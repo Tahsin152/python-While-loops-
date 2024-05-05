92a059866edd2aa734e14f23bd40c4c703ccbe2a
Tahsin Ahmed <159126363+Tahsin152@users.noreply.github.com>
2024-05-05 15:31:32 +0600

    Add files via upload

---
+++ python.py
@@ -0,0 +1,28 @@
+# count=5
+# while(count>0) :
+#     print(count)
+#     count=count-1
+
+#------------------------------------------------------------------------------------------
+
+# x=int(input("Enter a number :"))
+# while(x>0):
+#     print(x)
+#     x=x-1
+# if(x<0):
+#     print("The number is negative")
+# else:
+#     print("The number is zero")
+
+#--------------------------------------------------------------------------------------------------
+
+x=int(input("Enter a number :"))
+print(x,"is your first entered number")
+while(x<=12) :
+    x=int(input("Enter a number :"))
+    print("The number you entered is :",+x)
+else :
+    print("Done with the loop")
+
+
+
